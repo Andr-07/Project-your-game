e9217629d1b3e4d41616dba760216a22066b143f
filpoyma <filpoyma@gmail.com>
2019-06-07 17:06:15 +0300

    bootstrap

--- src/client/components/Quiz.jsx
+++ src/client/components/Quiz.jsx
@@ -1,42 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class Quiz extends React.Component {
 
     handler = (row, cell) => {
-        console.log(row, cell)
+        console.log(row, cell);
     }
 
 
     render() {
         return (
             <div>
-
-                <table border='2'>
+                <table class="table table-bordered text-center">
                     <tbody>
-                        {Array(3).fill(1).map((row, rowKey) =>
-                            <tr>
-                                {rowKey === 0 &&
-                                    <td>HTML</td>
-                                }
-                                {rowKey === 1 &&
-                                    <td>CSS</td>
-                                }
-                                {rowKey === 2 &&
-                                    <td>JS</td>
-                                }
-                                {Array(3).fill(1).map((cell, cellKey) =>
-                                    <td onClick={() => this.handler(rowKey, cellKey)}>
-                                        {/* {rowKey}{cellKey} */}
-                                        <Link to={`/game/${rowKey}/${cellKey}`}>{(cellKey + 1) * 50}</Link>
-                                    </td>
-                                )}
-                            </tr>
-                        )}
+                    {Array(3).fill(1).map((row, rowKey) =>
+                        <tr>
+                            {rowKey === 0 && <td><b>HTML</b></td>}
+                            {rowKey === 1 && <td><b>CSS</b></td>}
+                            {rowKey === 2 && <td><b>JS</b></td>}
+                            {Array(3).fill(1).map((cell, cellKey) =>
+                                <td onClick={() => this.handler(rowKey, cellKey)}>
+                                    {/* {rowKey}{cellKey} */}
+                                    <Link to={`/game/${rowKey}/${cellKey}`}>{((cellKey + 1) * 50) * 2}</Link>
+                                </td>
+                            )}
+                        </tr>
+                    )}
                     </tbody>
                 </table>
             </div>
-
         );
     }
 }
--- src/client/components/Topic.jsx
+++ src/client/components/Topic.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 
 class Topic extends React.Component {
@@ -10,28 +9,18 @@ class Topic extends React.Component {
         return this.props.questions[category].map((el) => el.prompt);
     }
 
-
     render() {
-
         return (
             <div>
-                
-                
-                {this.props.match.params.topic === '0' && 
-                <h1>HTML</h1>
-                }
-                {this.props.match.params.topic === '1' && 
-                <h1>CSS</h1>
-                }
-                {this.props.match.params.topic === '2' && 
-                <h1>JS</h1>
-                }
+                {this.props.match.params.topic === '0' && <h1>HTML</h1>}
+                {this.props.match.params.topic === '1' && <h1>CSS</h1>}
+                {this.props.match.params.topic === '2' && <h1>JS</h1>}
                 {this.showQuestions(`category${this.props.match.params.topic}`)[this.props.match.params.question]}
-            </div>
-        );
+
+
+
+            </div>);
     }
 }
-
-
 
 export default Topic;
